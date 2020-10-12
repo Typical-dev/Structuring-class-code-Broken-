e93b9a9ba7dc180d329bb95b41bbaaca6ef90fb1
Typical-dev <70384457+Typical-dev@users.noreply.github.com>
2020-10-12 20:04:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+var hypnoticBall, database;
+var position;
+var canvas;
+var gameState = 0;
+var playerCount = 0;
+var form;
+var player;
+var name;
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  canvas = createCanvas(400,400);
+
+  game = new Game();
+  game.getState();
+  game.start();
+
+}
+
+function draw(){
+  background("white");
+  
+    if(keyDown(LEFT_ARROW)){
+      writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+      writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+      writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+      writePosition(0,+1);
+    }
+    drawSprites();
+  
+}
+
+function writePosition(x,y){
+  database.ref('ball/position').set({
+    'x': position.x + x ,
+    'y': position.y + y
+  })
+}
+
+function readPosition(data){
+  position = data.val();
+  console.log(position.x);
+  hypnoticBall.x = position.x;
+  hypnoticBall.y = position.y;
+}
+
+function showError(){
+  console.log("Error in writing to the database");
+}
